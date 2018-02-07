4c1bbfb681ca626b008b3d8b6a66319664327516
geyherabide <geyherabide@geyherabide-hp-probook-450-g2.belatrix.com>
2018-02-07 11:46:28 -0300

    removing comments

--- src/scenes/MultipleChoice/data/reducer.js
+++ src/scenes/MultipleChoice/data/reducer.js
@@ -34,19 +34,6 @@ export const choiceLoadingReducer = (state = false, action) => {
   }
 };
 
-// export const choiceSaveReducer = (state = 0, action) => {
-//   switch (action.type) {
-//     case Actions.CHOICE_SAVE:
-//       return 0;
-//     case Actions.CHOICE_SAVE_SUCCESS:
-//       return action.payload;
-//     case Actions.CHOICE_SAVE_ERROR:
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// };
-
 export const choiceResultReducer = (state = { totalScore: 0, correctAnswers: [] }, action) => {
   switch (action.type) {
     case Actions.CHOICE_SAVE_SUCCESS:
